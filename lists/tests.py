f0c6f6589d459545fb07ae641913369b7a6d3285
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2021-03-11 16:55:50 +0700

    Сохранил на стр 99

--- lists/tests.py
+++ lists/tests.py
@@ -8,8 +8,14 @@ from django.http import HttpRequest
 class HomePageTest(TestCase):
 	'''тест на токсичность'''
 
-	def test_home_page_returns_correct_html(self):
+	def test_uses_home_template(self):
 		'''тест: используется домашний шаблон'''
 		response = self.client.get('/')
 
+		self.assertTemplateUsed(response, 'home.html')
+
+	def test_can_save_a_POST_request(self):
+		'''тест: можно сохранить пост запрос'''
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
