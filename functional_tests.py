e6bff0b2fb380df97d952aa7305ca8c696bbc613
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2021-03-11 17:19:36 +0700

    Небольшой рефакторинг (стр 100)

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
 		'''демонтаж'''
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		'''Подтверждение строки в таблице списка'''
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrive_it_later(self):
 		'''можно начать список и получить его позже'''
 
@@ -41,10 +47,8 @@ class NewVisitorTest(unittest.TestCase):
 		# страница содержит "1: Купить павлиньи перья" в качестве элемента списка
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
+		self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 
 		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
 		# Она вводит "Сделать мушку из павлиньих перьев" 
@@ -58,11 +62,8 @@ class NewVisitorTest(unittest.TestCase):
 		# Страница снова обновляется, и теперь показывает оба элемента ее списка
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-		self.assertIn(
-			 '2: Сделать мушку из павлиньих перьев',
-			 [row.text for row in rows]
-		)
+		self.check_for_row_in_list_table('1: Купить павлиньи перья')
+		self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
 		# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 		# сайт сгенерировал для нее уникальный URL-адрес – об этом
