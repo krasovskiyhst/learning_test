7205e657fe1912ceb615be09f1b29e52ed18a893
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2021-03-23 22:07:48 +0700

    Новый урл для отображения шаблона. Короч, стр 145

--- lists/tests.py
+++ lists/tests.py
@@ -20,28 +20,18 @@ class HomePageTest(TestCase):
 		self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')#КОД С ДУШКОМ: тест POST-запроса слишком длинный.
+		self.assertEqual(new_item.text, 'A new list item')#КОД С ДУШКОМ:тест POST-запроса слишком длинный.
 
 	def test_redirects_after_POST(self):
 		'''Переадресует после пост-запроса'''
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/one-of-a-kind-list-in-the-world/')
 
 	def test_only_saves_items_when_necessary(self):
 		'''тест: сохраняет элементы, только когда нужно'''
 		self.client.get('/')
 		self.assertEqual(Item.objects.count(), 0)
-
-	def test_displays_all_list_items(self):
-		'''тест: отображаются все элементы списка'''
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-
-		response = self.client.get('/')
-
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -65,3 +55,21 @@ class ItemModelTest(TestCase):
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+	'''тест представления списка'''
+
+	def test_uses_list_template(self):
+		'''тест: используется шаблон списка'''
+		response = self.client.get('/lists/one-of-a-kind-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+
+	def test_displays_all_items(self):
+		'''тест: отображаются все элементы списка'''
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/one-of-a-kind-list-in-the-world/')
+
+		self.assertContains(response, 'itemey 1')
+		self.assertContains(response, 'itemey 2')
