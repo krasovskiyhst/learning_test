e298966249d953f90f48166583e06e9afeab2b62
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2021-03-24 20:58:46 +0700

    str 150

--- lists/tests.py
+++ lists/tests.py
@@ -15,24 +15,6 @@ class HomePageTest(TestCase):
 
 		self.assertTemplateUsed(response, 'home.html')
 
-	def test_can_save_a_POST_request(self):
-		'''тест: можно сохранить пост запрос'''
-		self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')#КОД С ДУШКОМ:тест POST-запроса слишком длинный.
-
-	def test_redirects_after_POST(self):
-		'''Переадресует после пост-запроса'''
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/one-of-a-kind-list-in-the-world/')
-
-	def test_only_saves_items_when_necessary(self):
-		'''тест: сохраняет элементы, только когда нужно'''
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 	'''тест модели жлемента списка'''
@@ -73,3 +55,17 @@ class ListViewTest(TestCase):
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+	'''тест нового списка'''
+	def test_can_save_a_POST_request(self):
+		'''тест: можно сохранить post-запрос'''
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		'''тест: переадресует после post-запроса'''
+		response = self.client.post('/lists/new', data={'item_text': 'A newlist item'})
+		self.assertRedirects(response, '/lists/one-of-a-kind-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,14 @@ from lists.models import Item
 # Create your views here.
 def home_page(request):
 	'''домашняя страница'''
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/one-of-a-kind-list-in-the-world/')
 	return render(request, 'home.html')
 
 def view_list(request):
 	'''представление списка'''
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	'''новый список'''
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/one-of-a-kind-list-in-the-world/')
