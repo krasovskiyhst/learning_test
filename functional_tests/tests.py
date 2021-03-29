f6396c75be1284c5af6e74127de0e1e93541eebf
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2021-03-29 17:09:45 +0700

    Первые шаги ФТ для макетирования + стилевого фоормления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -31,6 +31,32 @@ class NewVisitorTest(LiveServerTestCase):
 				if time.time() - start_time > MAX_WAIT:
 					raise e
 				time.sleep(0.5)
+
+	def test_layout_and_styling(self):
+		'''тест макета и стилевого оформления'''
+		# Эдит открывает домашнюю страницу
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# Она видит что поле ввода находится по центру
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+
+		# Она начинает новый список и видит, что поле ввода там тоже
+		# аккуратно центировано
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+		 inputbox.location['x'] + inputbox.size['width'] / 2,
+		 512,
+		 delta=10
+		)
 		
 
 	def test_can_start_a_list_for_one_user(self):
